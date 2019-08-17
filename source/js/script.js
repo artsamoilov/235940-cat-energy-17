e22c16d2f023a931a9df206fadd9195d62707532
artsamoilov <art@htmlacademy.ru>
2019-08-17 16:21:22 +0300

    Исправляет стили в меню и форме

---
+++ source/js/script.js
@@ -0,0 +1,10 @@
+var btnMenu = document.querySelector(".main-nav__toggle");
+var btnMenuClose = document.querySelector(".main-nav__toggle--close");
+var popupMenu = document.querySelector(".site-list");
+
+btnMenu.classList.add("flex-show");
+popupMenu.classList.add("hide");
+
+if (btnMenu) {
+
+};
