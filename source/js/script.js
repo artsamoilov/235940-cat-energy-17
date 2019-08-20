954f47eddeb01fad8b902d3e6d0e0290a03d039d
Art <art@htmlacademy.ru>
2019-08-20 23:55:14 +0300

    Добавляет поддержку IE11 и облегчает скрипт

--- source/js/script.js
+++ source/js/script.js
@@ -1,20 +1,13 @@
 var btnMenu = document.querySelector(".main-nav__toggle--menu");
 var popupMenu = document.querySelector(".site-list");
 
-btnMenu.classList.add("main-nav__toggle--on");
-popupMenu.classList.add("site-list--hidden");
-
 if (btnMenu) {
+  btnMenu.classList.add("main-nav__toggle--on");
+  popupMenu.classList.add("site-list--hidden");
+
   btnMenu.addEventListener("click", function (evt) {
-    // evt.preventDefault();
-    if (btnMenu == document.querySelector(".main-nav__toggle--menu")) {
-      btnMenu.classList.add("main-nav__toggle--close");
-      btnMenu.classList.remove("main-nav__toggle--menu");
-      popupMenu.classList.remove("site-list--hidden");
-    } else if (btnMenu == document.querySelector(".main-nav__toggle--close")) {
-      btnMenu.classList.add("main-nav__toggle--menu");
-    btnMenu.classList.remove("main-nav__toggle--close");
-    popupMenu.classList.add("site-list--hidden");
-    };
+    btnMenu.classList.toggle("main-nav__toggle--close");
+    btnMenu.classList.toggle("main-nav__toggle--menu");
+    popupMenu.classList.toggle("site-list--hidden");
   });
 };
